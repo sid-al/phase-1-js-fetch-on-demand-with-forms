7161c751724397360ff2f412921e036b5f7a935e
sid-al <sendplusx@gmail.com>
2022-06-15 21:46:14 +0300

    commit tree 67ffc06fe50532007b1f03282ae7e6af5e4c593a

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+  inputForm.addEventListener('submit',(event)=>{
+
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+// 
